245166553b780fc7a29dc1fe826179eb8726cfdb
David Zucker <davzucky@hotmail.com>
2018-10-06 14:53:28 +0800

    commit updated hyperwavepathfinder

--- src/hyperwave/hyperwavepathfinder.py
+++ src/hyperwave/hyperwavepathfinder.py
@@ -1,23 +1,31 @@
-
-import pandas as pd
 import numpy as np
-import time
-from datetime import datetime, date, time, timedelta
+import pandas as pd
 from scipy.spatial import ConvexHull
 
 
-class Hyperwave_Path_Finder():
+class HyperwavePathFinder:
 
-    def get_hyperwave_path(self, df_input):
+    @staticmethod
+    def get_hyperwave_path(df_input: pd.DataFrame):
+        # Extract the hyperwave path using the ConvexHull.
+        # The df_input need to contain at least the following columns
+        #   - close
+        #   - date
+        #   - high
+        #   - is_price_closing_up
+        #   - low
+        #   - open
+        #   - weekId
+
         # Add the column with normalized value
-        df_input.loc[:, 'close_normalize'] = self.get_column_normalize_values(
+        df_input.loc[:, 'close_normalize'] = HyperwavePathFinder.get_column_normalize_values(
             df_input, 'close')
-        df_input.loc[:, 'weekId_normalize'] = self.get_column_normalize_values(
+        df_input.loc[:, 'weekId_normalize'] = HyperwavePathFinder.get_column_normalize_values(
             df_input, 'weekId')
 
         # Use convexHull to find the support lines
         hull = ConvexHull(
-            df_input[['weekId_normalize',  'close_normalize']].dropna())
+            df_input[['weekId_normalize', 'close_normalize']].dropna())
 
         hull_results = [[min(pair[0], pair[1]), max(pair[0], pair[1])]
                         for pair in hull.simplices]
@@ -33,65 +41,72 @@ class Hyperwave_Path_Finder():
                          "y2_normalize": df_input['close_normalize'].iloc[pair[1]]} for pair in hull_results]
 
         df = pd.DataFrame(data_from_to)
-        df['m'] = self.get_line_slope(df,
-                                      x1_col_name='x1',
-                                      y1_col_name='y1',
-                                      x2_col_name='x2',
-                                      y2_col_name='y2')
-        df['b'] = self.get_line_origine(df,
-                                        x1_col_name='x1',
-                                        y1_col_name='y1',
-                                        m_col_name='m')
-        df['m_normalize'] = self.get_line_slope(df,
-                                                x1_col_name='x1_normalize',
-                                                y1_col_name='y1_normalize',
-                                                x2_col_name='x2_normalize',
-                                                y2_col_name='y2_normalize')
-        df['b_normalize'] = self.get_line_origine(df,
-                                                  x1_col_name='x1_normalize',
-                                                  y1_col_name='y1_normalize',
-                                                  m_col_name='m_normalize')
+        df['m'] = HyperwavePathFinder.get_line_slope(df,
+                                                     x1_col_name='x1',
+                                                     y1_col_name='y1',
+                                                     x2_col_name='x2',
+                                                     y2_col_name='y2')
+        df['b'] = HyperwavePathFinder.get_line_origine(df,
+                                                       x1_col_name='x1',
+                                                       y1_col_name='y1',
+                                                       m_col_name='m')
+        df['m_normalize'] = HyperwavePathFinder.get_line_slope(df,
+                                                               x1_col_name='x1_normalize',
+                                                               y1_col_name='y1_normalize',
+                                                               x2_col_name='x2_normalize',
+                                                               y2_col_name='y2_normalize')
+        df['b_normalize'] = HyperwavePathFinder.get_line_origine(df,
+                                                                 x1_col_name='x1_normalize',
+                                                                 y1_col_name='y1_normalize',
+                                                                 m_col_name='m_normalize')
         df['angle'] = np.rad2deg(np.arctan2(df['m'], 1))
         df['angle_normalize'] = np.rad2deg(
             np.arctan2(df['m_normalize'], 1))
         df['weeks'] = np.abs(df['x1'] - df['x2'])
         df['mean_error'] = df.apply(
-            lambda row: self.calculate_mean_error(row, df_input), axis=1)
+            lambda row: HyperwavePathFinder.calculate_mean_error(row, df_input), axis=1)
         df['nb_is_lower'] = df.apply(
-            lambda row: self.nb_cut_price_low(row, df_input), axis=1)
+            lambda row: HyperwavePathFinder.nb_cut_price_low(row, df_input), axis=1)
         df['ratio_error_cut'] = df['mean_error'] / df['nb_is_lower']
-        df['ratio_slope_y1_normalize'] = df['y1_normalize']/df['m_normalize']
-        df['ratio_slope_y2_normalize'] = df['y2_normalize']/df['m_normalize']
+        df['ratio_slope_y1_normalize'] = df['y1_normalize'] / df['m_normalize']
+        df['ratio_slope_y2_normalize'] = df['y2_normalize'] / df['m_normalize']
         return df
 
-    def get_line_slope(self, df, x1_col_name='x1', y1_col_name='y1', x2_col_name='x2', y2_col_name='y2'):
+    @staticmethod
+    def get_line_slope(df, x1_col_name='x1', y1_col_name='y1', x2_col_name='x2', y2_col_name='y2'):
         return (df[y1_col_name] - df[y2_col_name]) / (df[x1_col_name] - df[x2_col_name])
 
-    def get_line_origine(self, df, x1_col_name='x1', y1_col_name='y1', m_col_name='m'):
+    @staticmethod
+    def get_line_origine(df, x1_col_name='x1', y1_col_name='y1', m_col_name='m'):
         return df[y1_col_name] - (df[x1_col_name] * df[m_col_name])
 
-    def get_column_normalize_values(self, df, column_name='close'):
-        column_name_normalize = '{}_normalize'.format(column_name)
+    @staticmethod
+    def get_column_normalize_values(df, column_name='close'):
         max_value = df.loc[df[column_name].idxmax()][column_name]
         min_value = df.loc[df[column_name].idxmin()][column_name]
 
         return (df[column_name] - min_value) / (max_value - min_value)
 
-    def get_mean_error(self, y_true, y_pred):
+    @staticmethod
+    def get_mean_error(y_true, y_pred):
         y_square_diff = y_true - y_pred
         return np.sum(y_square_diff) / len(y_true)
 
-    def nb_is_lower(self, y_true, y_pred):
+    @staticmethod
+    def nb_is_lower(y_true, y_pred):
         lower_item = y_true[y_true <= y_pred]
         return len(lower_item)
 
-    def get_y(self, x, m, b):
+    @staticmethod
+    def get_y(x, m, b):
         return x * m + b
 
-    def calculate_mean_error(self, row, df):
-        y_pred = self.get_y(df['weekId'], row['m'], row['b'])
-        return self.get_mean_error(df['close'], y_pred)
+    @staticmethod
+    def calculate_mean_error(row, df):
+        y_pred = HyperwavePathFinder.get_y(df['weekId'], row['m'], row['b'])
+        return HyperwavePathFinder.get_mean_error(df['close'], y_pred)
 
-    def nb_cut_price_low(self, row, df):
-        y_pred = self.get_y(df['weekId'], row['m'], row['b'])
-        return self.nb_is_lower(df['low'], y_pred)
+    @staticmethod
+    def nb_cut_price_low(row, df):
+        y_pred = HyperwavePathFinder.get_y(df['weekId'], row['m'], row['b'])
+        return HyperwavePathFinder.nb_is_lower(df['low'], y_pred)
