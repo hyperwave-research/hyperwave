defbac6b9f7bea6569258bf249e9cf79f30afce2
David Zucker <davzucky@hotmail.com>
2018-10-14 10:24:57 +0800

    fix dependency problem

--- src/hyperwave/__init__.py
+++ src/hyperwave/__init__.py
@@ -1,10 +1,16 @@
 """Minimal Project Task Management."""
 
+from .hyperwave_grouping import (
+    HyperwaveGrouping,
+    HyperwaveWeekLenghtGrouping,
+    HyperwavePhaseGrouper,
+    HyperwaveGroupingPhasePercent,
+    HyperwaveGroupingPhaseAggregator
+)
+
 from .hyperwave import (  # noqa: F401
     Hyperwave
 )
-from hyperwave.hyperwave_grouping import HyperwaveGrouping, HyperwaveWeekLenghtGrouping, HyperwavePhaseGrouper, \
-    HyperwaveGroupingPhasePercent, HyperwaveGroupingPhaseAggregator
 
 from .ohlc_loader import  (  # noqa: F401
     OhlcLoader,
