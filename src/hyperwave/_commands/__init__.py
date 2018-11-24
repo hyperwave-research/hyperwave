d648e08080f7e506d3519cf57f9b5b2d40c79530
David Zucker <davzucky@hotmail.com>
2018-11-24 19:59:55 +0800

    add new command download-index

--- src/hyperwave/_commands/__init__.py
+++ src/hyperwave/_commands/__init__.py
@@ -2,9 +2,11 @@
 from .download import set_command as download_set_command
 from .check import set_command as check_set_command
 from .check_index import set_command as check_index_command
+from .download_index import set_command as download_index_command
 
 commands = [
     download_set_command,
     check_set_command,
-    check_index_command
+    check_index_command,
+    download_index_command
 ]
