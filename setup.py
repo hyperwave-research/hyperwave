0da2fd605878ca8093d070521a64bf10b8767832
David Zucker <davzucky@hotmail.com>
2019-02-04 16:42:00 +0800

    Add pytz as dependency to manage time zone

--- setup.py
+++ setup.py
@@ -3,25 +3,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hyperwave',
-    version='0.1',
-    license='MIT',
-    description='hyperwave research',
-
-    author='David Zucker',
-    author_email='david@programmableproduction.com',
-    url='http://blog.programmableproduction.com',
-
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-
-    install_requires=['numpy', 'pandas', 'scipy', 'requests', 'tqdm'],
+    name="hyperwave",
+    version="0.1",
+    license="MIT",
+    description="hyperwave research",
+    author="David Zucker",
+    author_email="david@programmableproduction.com",
+    url="http://blog.programmableproduction.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["numpy", "pandas", "scipy", "requests", "tqdm", "pytz"],
     extras_require={"dev": [["pytest", "pytest-runner"]]},
     entry_points={
-        'console_scripts': [
-            'hw = hyperwave.main:Main',
-            'consensio = consensio.main:Main',
-            'marketdata = marketdata.main:Main',
+        "console_scripts": [
+            "hw = hyperwave.main:Main",
+            "consensio = consensio.main:Main",
+            "marketdata = marketdata.main:Main",
         ]
     },
 )
