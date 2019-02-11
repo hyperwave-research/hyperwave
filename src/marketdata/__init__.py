3234282e72ee790cf60e9688f61bafddc1e3c666
David Zucker <davzucky@hotmail.com>
2019-02-11 12:13:38 +0800

    Black reformat

--- src/marketdata/__init__.py
+++ src/marketdata/__init__.py
@@ -4,8 +4,6 @@ from .ohlc_loader import Loader
 
 from .groups import Groups, GroupComposition, TickerInfo
 
-# from .transform import ResultSet, split_column_to_ohlc, get_ohlc_for_column, resample_data
-
 from .main import Main
 
 __version__ = "0.1.0"
