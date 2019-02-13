a2004cc04cc2e460d89a4b847c58e1de8d7d1290
David Zucker <davzucky@hotmail.com>
2019-02-13 08:59:27 +0800

    Reformat with back

--- src/marketdata/_commands/__init__.py
+++ src/marketdata/_commands/__init__.py
@@ -3,4 +3,9 @@ from .download_index import set_command as download_index_command
 from .split_assets import set_command as split_asset_command
 from .resample import set_command as resample_command
 
-commands = [download_set_command, download_index_command, split_asset_command, resample_command]
+commands = [
+    download_set_command,
+    download_index_command,
+    split_asset_command,
+    resample_command,
+]
